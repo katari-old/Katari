54fab6c3b8080a48f673a6607b02dac504a7cce8
Pablo Gra\~na <pablo.pg@gmail.com>
2010-09-16 11:13:47 +0000

    New module katari-jsmodule.
    This is the first commit, just the module generated from the archetype.

---
+++ dev/katari-jsmodule/src/main/resources/com/globant/katari/jsmodule/view/hello.ftl
@@ -0,0 +1,14 @@
+<html>
+
+  <head>
+    <title>Hello</title>
+  </head>
+
+  <body>
+    <h3>Hello ${userName}</h3>
+
+    <!-- vim: set ts=2 sw=2 et ai: -->
+  </body>
+
+</html>
+
---
+++ dev/katari-jsmodule/src/test/java/com/globant/katari/jsmodule/view/HelloControllerTest.java
@@ -0,0 +1,23 @@
+/* vim: set ts=2 et sw=2 cindent fo=qroca: */
+
+package com.globant.katari.jsmodule.view;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloControllerTest {
+
+  @Test
+  public void testHandleRequestInternal() throws Exception {
+    HelloController controller = new HelloController();
+    ModelAndView mav = controller.handleRequestInternal(
+        new MockHttpServletRequest(), new MockHttpServletResponse());
+    assertThat(mav.getViewName(), is("hello"));
+  }
+}
+
