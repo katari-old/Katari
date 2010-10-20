1f35b6b5d9248c113bdcfb5981f44c6f764f804b
Pablo Gra\~na <pablo.pg@gmail.com>
2010-10-20 21:19:42 +0000

    Removed an usused import.

--- dev/katari-gadget-container/src/main/java/com/globant/katari/gadgetcontainer/application/ListApplicationsCommand.java
+++ dev/katari-gadget-container/src/main/java/com/globant/katari/gadgetcontainer/application/ListApplicationsCommand.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.LinkedList;
 
 import com.globant.katari.core.application.Command;
-import com.globant.katari.core.application.JsonRepresentation;
 
 import com.globant.katari.gadgetcontainer.domain.ApplicationRepository;
 import com.globant.katari.gadgetcontainer.domain.GadgetGroupRepository;
