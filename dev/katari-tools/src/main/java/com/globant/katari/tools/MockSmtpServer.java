3f99c871b039fc9c79bd3e9316aac7e5e1f3b1dd
Pablo Gra\~na <pablo.pg@gmail.com>
2010-11-15 02:31:00 +0000

    New mock smtp server.
    An smtp server that can be run stand alone and outputs the sent email to
    the console.
    
    Thanks Emiliano Arango.

---
+++ dev/katari-tools/src/main/java/com/globant/katari/tools/MockSmtpServer.java
@@ -0,0 +1,92 @@
+/* vim: set ts=2 et sw=2 cindent fo=qroca: */
+
+package com.globant.katari.tools;
+
+import java.util.Iterator;
+
+import com.dumbster.smtp.SmtpMessage;
+
+/** A mock smtp server that does not deliver mail.
+ *
+ * This is used to start an smtp server that simply logs all received mail to
+ * the terminal.
+ *
+ * It is intended to be started from the command line with:
+ *
+ * mvn exec:java -Dexec.mainClass=com.globant.katari.tools.MockSmtpServer
+ *
+ * The server accepts two parameters: the port to listen on, and the dump
+ * interval, in milliseconds. If no port number is specified, it listens on
+ * port 3525.  If no dump interval is specified, it dumps the received email
+ * every 5 seconds.
+ *
+ * It will start an smtp that will dump the received emails every 5 seconds.
+ *
+ * @author waabox (emiliano[dot]arango[at]globant[dot]com)
+ */
+public final class MockSmtpServer {
+
+  /** Will dump the received emails after this many milliseconds. */
+  private static final int MAIL_DUMP_INTERVAL = 5000;
+
+  /** Will listen by default on this port. */
+  private static final int DEFAULT_PORT = 3525;
+
+  /** The smtp server, null until the call to main.
+   */
+  private static DummySmtpServer server = null;
+
+  /** Private constructor, no instances of this class should be created.
+   */
+  private MockSmtpServer() {
+  }
+
+  /** Entry point to start the mail server.
+   *
+   * @param args It expects the port number (optional, defaults to 3525), and
+   * the dump interval (optional, defaults to 5 seconds) in milliseconds.
+   *
+   * @throws Exception in case of error.
+   */
+  public static void main(final String[] args) throws Exception {
+    int port = DEFAULT_PORT;
+    int dumpInterval = MAIL_DUMP_INTERVAL;
+
+    if(args.length > 0) {
+      port = Integer.valueOf(args[0]);
+    }
+    if(args.length > 1) {
+      dumpInterval = Integer.valueOf(args[1]);
+    }
+
+    server = DummySmtpServer.start(port);
+    System.out.println("Smtp server started, listening on port: "
+        + server.getPortNumber());
+    int lastSize = 0;
+    while(true) {
+      if(server.getReceivedEmailSize() > lastSize) {
+        Iterator<SmtpMessage> it = server.iterator();
+        while (it.hasNext()) {
+          SmtpMessage smtpMessage = (SmtpMessage) it.next();
+          System.out.println("###################################");
+          System.out.println(smtpMessage.getHeaderValue("Subject"));
+          System.out.println("-----------------------------------");
+          System.out.println(smtpMessage.getBody());
+          System.out.println("###################################");
+        }
+      }
+      lastSize = server.getReceivedEmailSize();
+      Thread.sleep(dumpInterval);
+    }
+  }
+
+  /** Stops the server, only useful when testing this class.
+   */
+  static void stop() {
+    if (server != null) {
+      server.stop();
+      server = null;
+    }
+  }
+}
+
---
+++ dev/katari-tools/src/test/java/com/globant/katari/tools/MockSmtpServerTest.java
@@ -0,0 +1,106 @@
+/* vim: set ts=2 et sw=2 cindent fo=qroca: */
+
+package com.globant.katari.tools;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+
+public class MockSmtpServerTest {
+
+  PrintStream originalOut;
+  
+  ByteArrayOutputStream dataSent;
+
+  int port;
+
+  @Before
+  public void setUp() throws IOException {
+    originalOut = System.out;
+    dataSent = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(dataSent);
+    System.setOut(ps);
+    ServerSocket serverSocket = new ServerSocket(0);
+    port = serverSocket.getLocalPort();
+    serverSocket.setReuseAddress(true);
+    serverSocket.close();
+  }
+
+  @After
+  public void tearDown() {
+    if (originalOut != null) {
+      System.setOut(originalOut);
+    }
+  }
+
+  @Test
+  public void testSend() throws Exception {
+    Thread smtpThread = new Thread(new Runnable() {
+      public void run() {
+        try {
+          MockSmtpServer.main(new String[]{String.valueOf(port), "1"});
+        } catch (Exception e) {
+        }
+        return;
+      }
+    });
+    smtpThread.start();
+
+    int count = 5;
+    while( ! isOpened(port) && count > 0) {
+      count --;
+      Thread.sleep(10);
+    }
+
+    // send a mail and verify that it is received.
+    Properties props = new Properties();
+    props.put("mail.smtp.host", "localhost");
+    props.put("mail.smtp.port", String.valueOf(port));
+
+    Session mailSession = Session.getInstance(props);
+    Message sentMessage = new MimeMessage(mailSession);
+    sentMessage.setFrom(new InternetAddress("from@blah"));
+    sentMessage.setRecipient(RecipientType.TO, new InternetAddress("to@blah"));
+    sentMessage.setSubject("the subject");
+    sentMessage.setText("the body");
+    Transport.send(sentMessage);
+
+    Thread.sleep(500);
+    
+    assertThat(dataSent.toString(), containsString("the subject"));
+    assertThat(dataSent.toString(), containsString("the body"));
+  }
+
+  public static boolean isOpened(int port) throws IOException {
+    boolean result = false;
+    ServerSocket ssocket = null;
+    try {
+      ssocket = new ServerSocket(port);
+      return false;
+    } catch (IOException ex) {
+      return true;
+    } finally {
+      if (ssocket != null) {
+        ssocket.close();
+      }
+    }
+  }
+}
+
