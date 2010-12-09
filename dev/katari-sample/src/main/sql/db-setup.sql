d93a1d9a8fadf92b83576dfefb7dd312271d1ca1
Pablo Gra\~na <pablo.pg@gmail.com>
2010-12-09 03:49:09 +0000

    Added links to showcase the different types of menus to the home page.

--- dev/katari-sample/src/main/sql/db-setup.sql
+++ dev/katari-sample/src/main/sql/db-setup.sql
@@ -1,21 +1,36 @@
 -- vim: set ts=2 et sw=2:
 
-insert into users(id, name, email, password, user_type, is_active) values (1, 'admin',
-  'admin@none', 'admin', 'user', true);
+insert into users(id, name, email, password, user_type, is_active) values (
+  1, 'admin', 'admin@none', 'admin', 'user', true);
 insert into roles(id, name) values (1, 'ADMINISTRATOR');
 insert into roles(id, name) values (2, 'REPORT_ADMIN');
 insert into users_roles(users_id, roles_id) select users.id, roles.id
   from users, roles where users.name='admin' and roles.name='ADMINISTRATOR';
 
 -- Create the inital home page.
-insert into pages(id, site_name, name, title, content, modifier,
-  publication_date) values
-  (1, 'default', 'home','home_title', '<p>Welcome to <b>Katari</b>. This is
-    the home page. It can be edited if you have the right
-    privileges.</p>','admin', now());
+insert into pages
+  (id, site_name, name, title, content, modifier, publication_date)
+  values
+  (1, 'default', 'home','home_title',
+    '<p>Welcome to <b>Katari</b>. This is the home page. It can be edited if
+    you have the right privileges.</p>
+    <p>Select the style of menu that you want:
+    <ul>
+      <li>
+        <a href="home?menuType=classic">Classic menu</a>
+      </li>
+      <li>
+        <a href="home?menuType=bar">Javascript based menu bar</a>
+      </li>
+      <li>
+        <a href="home?menuType=context">Javascript based context-like menu</a>
+      </li>
+    </ul>
+    ','admin', now());
 
 -- Create a sample about page.
-insert into pages(id, site_name, name, title, content, modifier, publication_date) values
+insert into pages(id, site_name, name, title, content, modifier,
+  publication_date) values
   (2,'default', 'about','about_title', '<p>This is an about page example.</p>',
     'admin', now());
 
